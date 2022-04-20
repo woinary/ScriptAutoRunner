4cf76206fa991b5d336d6cfdff3a2ee61acc0e68
Yoshiaki Yamaguchi <yoshiaki.gh@pawagumo.net>
2022-04-21 08:53:23 +0900

    * ボタンを表示する位置をSPSメニューバー部分に変更 * 変更部分の検索方法を変更（クラス名は変動してしまうため、h2タグに）

--- common/add_link_target.js
+++ common/add_link_target.js
@@ -1,8 +1,8 @@
 function forceAthoerTabForLink() {
     // リンク集部分を探す
-    const elmLinkCollection = document.getElementsByClassName('ay_e_271d94b8');
+    const elmLinkCollection = document.getElementsByTagName('h2')[0].parentNode;
     // リンク集の中のAタグを処理する
-    Array.prototype.forEach.call(elmLinkCollection[0].getElementsByTagName('a'), x => {
+    Array.prototype.forEach.call(elmLinkCollection.getElementsByTagName('a'), x => {
         if (x.textContent.match(/❒/) == null) {
             // 別タブで開くための属性追加
             x.setAttribute('target', '_blank');
@@ -13,7 +13,7 @@ function forceAthoerTabForLink() {
 }
 
 // SPSのタイトル部分を探してボタン追加
-const siteHeader = document.getElementsByClassName('title-53');
-const btnLink = siteHeader[0].parentNode.appendChild(document.createElement('button'));
+const siteHeader = document.getElementsByClassName('ms-Button--commandBar');
+const btnLink = siteHeader[0].parentNode.parentNode.appendChild(document.createElement('button'));
 btnLink.textContent = '↗️';
 btnLink.addEventListener("click",function(){forceAthoerTabForLink()});
